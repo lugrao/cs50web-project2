6dbf86f908839768f5ad2c6770750f5dc094f0ff
e <elugrao@gmail.com>
2021-10-27 16:16:45 -0300

    commit

--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,27 @@
+# from typing_extensions import ParamSpecKwargs
+from typing import List
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.forms import ModelForm
 
-from .models import User
+from .models import User, Category, Listing, Watchlist, Comment, Bid
+
+
+class ListingForm(ModelForm):
+    class Meta:
+        model = Listing
+        fields = ["title", "description",
+                  "category", "image_url", "starting_bid"]
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    # listings = 
+    return render(request, "auctions/index.html", {
+        "listings": Listing.objects.exclude(available=False).all() 
+    })
 
 
 def login_view(request):
@@ -61,3 +74,20 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create_listing(request):
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            user = User.objects.get(id=request.user.id)
+            partial_listing = Listing(seller=user, available=True)
+            listing = ListingForm(request.POST, instance=partial_listing)
+            listing.save()
+            return HttpResponseRedirect(reverse("index"))
+
+    print(request.user.id)
+    form = ListingForm()
+    return render(request, "auctions/create-listing.html", {
+        "form": form
+    })
